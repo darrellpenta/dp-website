af1bd7d2a6b9aa459c2f361a60022c37f425a9f7
Darrell Jonathan Penta <d.penta@neu.edu>
2017-09-22 09:53:18 -0400

    Cleaning directories/restructuring

--- scripts/prefixer.R
+++
@@ -1,6 +0,0 @@
-fixer<-function(x){
-  writeLines(unlist(lapply(c("-moz-","-webkit-","-ms-","-o-",""),paste0,x,collapse="\n")),sep="\n")
-}
-
-fixer(c("transform: scale3d(0.0,0.0,0.0);",
-	"transform: scale(0.0, 0.0) scaleZ(0.0);"))
